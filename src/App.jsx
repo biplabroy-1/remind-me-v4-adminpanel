7387e2c25b97f4d7b63ab660745cdd1cffa15b88
Biplab Roy <biplabr119201@gmail.com>
2024-10-10 22:57:11 +0530

    updated path

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,22 @@ const App = () => {
   return (
     <Router>
       <div>
-        <h1 className='text-5xl font-bold text-center p-4 text-blue-600'>Class Schedule Admin Dashboard</h1>
+        <h1 className="text-5xl font-bold text-center p-4 text-blue-600">
+          Class Schedule Admin Dashboard
+        </h1>
 
         <nav className="flex justify-center space-x-4 mb-4">
-          <Link to="/schedule" className="text-blue-500">Schedule Manager</Link>
-          <Link to="/holiday" className="text-blue-500">Holiday Manager</Link>
+          <Link to="/" className="text-blue-500">
+            Schedule Manager
+          </Link>
+          <Link to="/holiday" className="text-blue-500">
+            Holiday Manager
+          </Link>
         </nav>
 
         <Routes>
-          <Route path="/schedule" element={<ScheduleForm />} />
+          <Route path="/" element={<ScheduleForm />} />
           <Route path="/holiday" element={<HolidayManager />} />
-          <Route path="/" element={<Navigate to="/schedule" />} /> {/* Redirecting to /schedule */}
         </Routes>
       </div>
     </Router>
